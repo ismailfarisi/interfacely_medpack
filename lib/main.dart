344e0183ca32372cb12f592bc1aa17b5ec4374d9
ismail farisi <ismailfarisi@gmail.com>
2021-09-29 18:17:42 +0400

    initial

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: TextButton(
+            onPressed: () {},
+            child: Text("menu"),
+          ),
+          actions: [Text("Med"), Text("Pack")],
+        ),
+      ),
+    );
+  }
+}
